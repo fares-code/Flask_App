d2b00ce19344cad1852bcd1930df174fb3332208
fares-code <113051677+fares-code@users.noreply.github.com>
2025-05-10 03:33:53 +0300

    Make Update

--- app.py
+++ app.py
@@ -5,15 +5,45 @@ import numpy as np
 import os
 
 app = Flask(__name__)
-CORS(app)  # <-- This enables CORS for all routes
+CORS(app)  # This enables CORS for all routes
+
 
 model = joblib.load('Model.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # استلام البيانات المدخلة (الـ JSON)
     data = request.json
-    input_features = data['input']
-    prediction = model.predict([input_features])
+    
+    # استخراج البيانات من الـ JSON
+    input_data = [
+        data['HighBP'],
+        data['HighChol'],
+        data['CholCheck'],
+        data['BMI'],
+        data['Smoker'],
+        data['Stroke'],
+        data['HeartDiseaseorAttack'],
+        data['Veggies'],
+        data['HvyAlcoholConsump'],
+        data['AnyHealthcare'],
+        data['NoDocbcCost'],
+        data['GenHlth'],
+        data['MentHlth'],
+        data['PhysHlth'],
+        data['Sex'],
+        data['Age'],
+        data['Education'],
+        data['Income']
+    ]
+    
+    # تحويل البيانات إلى مصفوفة NumPy
+    input_features = np.array(input_data).reshape(1, -1)  # تحويل المدخلات إلى مصفوفة صف واحد
+    
+    # تنفيذ التنبؤ باستخدام النموذج
+    prediction = model.predict(input_features)
+    
+    # إرجاع النتيجة في شكل JSON
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
